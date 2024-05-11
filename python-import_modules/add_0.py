486a262049a4448e678ee90b5589b7b27c54384d
alexpr42 <8988@holbertonstudents.com>
2024-05-10 21:54:44 -0400

    add_0.py

---
+++ python-import_modules/add_0.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+    Args:
+        a: integer
+        b:  integer
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
