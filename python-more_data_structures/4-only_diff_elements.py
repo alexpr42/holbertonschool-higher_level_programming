52f75f4a4642f8924d79a016d2baea0983cd2fa6
alexpr42 <8988@holbertonstudents.com>
2024-05-14 22:55:07 -0400

    4-only_diff_elements.py

---
+++ python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 4-only_diff_elements.py
+
+
+def only_diff_elements(set_1, set_2):
+    valuediff = [value for value in set_1 if value not in set_2] + [value for value in set_2 if value not in set_1]
+    return valuediff
