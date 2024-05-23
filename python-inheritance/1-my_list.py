3ac7132ce2ada68ce364618f6c785d97538aff77
alexpr42 <8988@holbertonstudents.com>
2024-05-22 23:52:00 -0400

    1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
+"""Defines a class called MyList."""
+
+
 class MyList(list):
+    """
+    A subclass of list that provides an additional
+    method to print the list sorted.
+    """
+
     def print_sorted(self):
-        print(sorted(self))
+        """
+        Prints the list in sorted order without
+        modifying the original list.
+        """
+
+        sorted_list = sorted(self)
+        print(sorted_list)
