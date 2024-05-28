f5b8b9392faa8724f2157d70d69059a9818f5b57
alexpr42 <8988@holbertonstudents.com>
2024-05-28 12:02:29 -0400

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 # 0-read_file.py
 
+
 """function that print text file in utf-8 format"""
+
+
 def read_file(filename=""):
     """func that print text from other file"""
     with open(filename, encoding="utf-8") as f:
