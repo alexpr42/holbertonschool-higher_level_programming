3a5834dd65862b4bb7c9e7d77ccf33109e31409c
alexpr42 <8988@holbertonstudents.com>
2024-05-28 11:51:02 -0400

    0-read_file.py fix

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 # 0-read_file.py
 
-
+"""function that print text file in utf-8 format"""
 def read_file(filename=""):
-    # func that print text from other file
-
+    """func that print text from other file"""
     with open(filename, encoding="utf-8") as f:
-        # read the data
         read_data = f.read()
