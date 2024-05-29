a799f2017b1a4705b25d34b9355984071cc392ac
alexpr42 <8988@holbertonstudents.com>
2024-05-28 22:14:49 -0400

    8-class_to_json.py

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""function that returns the dictionary description with simple data structure"""
+
+
+def class_to_json(obj):
+    """
+    Convert a Python object to a dictionary representation.
+
+    Args:
+        obj: The object to be converted.
+
+    Returns:
+        A dictionary representation of the object.
+    """
+    return obj.__dict__
