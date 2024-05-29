ee29344213f39dbd127fccae96f58a06024e8d3d
alexpr42 <8988@holbertonstudents.com>
2024-05-28 22:51:05 -0400

    12-pascal_triangle.py fix

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -3,18 +3,19 @@
 
 
 def pascal_triangle(n):
+    """funtion that creat a pascal triangle"""
     if n <= 0:
         return []
 
-    triangle = [[1]]  # Iniciamos el triángulo con la primera fila
+    triangle = [[1]]  # triangle initiation
 
     for i in range(1, n):
         prev_row = triangle[-1]
-        new_row = [1]  # Cada nueva fila comienza con 1
-        # Construimos los elementos de la nueva fila
+        new_row = [1]  # new row
+        # new row contruction
         for j in range(1, i):
             new_row.append(prev_row[j - 1] + prev_row[j])
-        new_row.append(1)  # Cada nueva fila termina con 1
+        new_row.append(1)  # add to new lilne
         triangle.append(new_row)
 
     return triangle
