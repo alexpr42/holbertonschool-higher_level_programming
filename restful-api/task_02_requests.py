885eed8e451b000a8266969b451692d27baab8a3
alexpr42 <8988@holbertonstudents.com>
2024-06-01 20:02:04 -0400

    task_02_requests.py

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,33 +1,49 @@
 #!/usr/bin/python3
-"""make a request and work with json data, finally transfer to csv file"""
+"""use requests function in order to get info"""
+
 
 import requests
 import csv
 
 def fetch_and_print_posts():
+    # Fetch posts from JSONPlaceholder
     response = requests.get("https://jsonplaceholder.typicode.com/posts")
-    print(f"Status code: {response.status_code}")
-
+    
+    # Print the status code of the response
+    print("Status Code: {}".format(response.status_code))
+    
     if response.status_code == 200:
+        # Parse the fetched data into a JSON object
         posts = response.json()
+        
+        # Iterate through the parsed JSON data and print the titles of all posts
         for post in posts:
             print(post['title'])
     else:
         print("Failed to fetch posts")
 
-fetch_and_print_posts()
-
 def fetch_and_save_posts():
+    # Fetch posts from JSONPlaceholder
     response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    
     if response.status_code == 200:
+        # Parse the fetched data into a JSON object
         posts = response.json()
-        posts_list = [{"id": post["id"], "title": post["title"], "body": post["body"]} for post in posts]
-
-        with open('posts.csv', mode='w', newline='') as file:
-            writer = csv.DictWriter(file, fieldnames=["id", "title", "body"])
+        
+        # Structure the data into a list of dictionaries
+        posts_list = [
+            {"id": post["id"], "title": post["title"], "body": post["body"]}
+            for post in posts
+        ]
+        
+        # Write data into a CSV file
+        with open('posts.csv', 'w', newline='') as csvfile:
+            fieldnames = ['id', 'title', 'body']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            
             writer.writeheader()
             writer.writerows(posts_list)
+        
+        print("Posts have been saved to posts.csv")
     else:
         print("Failed to fetch posts")
-
-fetch_and_save_posts()
