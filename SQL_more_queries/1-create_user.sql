d1dc288cf21d515247b63136ed0ca797dd7e1d2b
alexpr42 <8988@holbertonstudents.com>
2024-06-18 23:14:20 -0400

    1-create_user.sql

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- Create the user user_0d_1 if it does not already exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to the user user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Apply the changes
+FLUSH PRIVILEGES;
