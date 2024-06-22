0ab8c653ef4fb0ed099ee92b8375cfed43fb7025
alexpr42 <8988@holbertonstudents.com>
2024-06-21 23:02:34 -0400

    1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,12 +1,12 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 This script lists all states with a name starting with 'N' (upper N)
 from the database hbtn_0e_0_usa.
 
-The script takes 3 arguments: mysql username, mysql password, and
-database name. It connects to a MySQL server running on localhost
-at port 3306, retrieves all states starting with 'N', and displays
-them sorted by state id in ascending order.
+The script takes 3 arguments: mysql username, mysql password
+, and database name. It connects to a MySQL server running
+on localhost at port 3306, retrieves all states starting wit
+h 'N', and displays them sorted by state id in ascending order.
 """
 
 import MySQLdb
@@ -33,8 +33,9 @@ def main():
 
     # Execute the SQL query to retrieve states starting with 'N'
     query = (
-            "SELECT id, name FROM states WHERE BINARY name LIKE 'N%'
-            ORDER BY id ASC")
+        "SELECT id, name FROM states WHERE BINARY name LIKE 'N%' "
+        "ORDER BY id ASC"
+    )
     cursor.execute(query)
 
     # Fetch all the rows from the executed query
@@ -51,3 +52,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
