e7e58d4094367568a4af30d2e38715417ece94bd
alexpr42 <8988@holbertonstudents.com>
2024-06-22 19:19:25 -0400

    9-model_state_filter_a.py fix

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -22,7 +22,8 @@ if __name__ == "__main__":
     session = Session()
 
     # Query State objects that contain the letter 'a' in their name
-    states_with_a = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+    states_with_a = session.query(State).filter(State.name.like('%a%'))
+    .order_by(State.id).all()
 
     # Print the results
     for state in states_with_a:
