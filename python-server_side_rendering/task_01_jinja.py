3b453f70da8fd923b1d16de647341594b23d3b3f
alexpr42 <8988@holbertonstudents.com>
2024-07-16 21:20:19 -0400

    fix

--- python-server_side_rendering/task_01_jinja.py
+++ python-server_side_rendering/task_01_jinja.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, jsonify
-import json
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -14,12 +13,6 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
-
-@app.route('/items')
-def items():
-    with open('items.json') as f:
-        data = json.load(f)
-    return render_template('items.html', items=data.get('items', []))
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
