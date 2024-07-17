238b48ba008bd9347204e311d724641614a03c4b
alexpr42 <8988@holbertonstudents.com>
2024-07-17 01:08:03 -0400

    all files

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -17,10 +17,13 @@ def contact():
 
 @app.route('/items')
 def items():
-    with open('items.json') as f:
-        data = json.load(f)
-    return render_template('items.html', items=data['items'])
-
+    try:
+        with open('items.json') as f:
+            data = json.load(f)
+            items = data.get('items', [])
+        return render_template('items.html', items=items)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return render_template('items.html', items=[])
 if __name__ == '__main__':
         app.run(debug=True, port=5000)
 
